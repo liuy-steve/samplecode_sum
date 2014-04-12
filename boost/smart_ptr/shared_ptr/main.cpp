46c140869314f99d04a44fa634fd0144b235ce76
liuyang <liuy_steve@hotmail.com>
2014-04-12 17:23:40 +0800

    reset sample of shared_ptr

--- boost/smart_ptr/shared_ptr/main.cpp
+++ boost/smart_ptr/shared_ptr/main.cpp
@@ -1,37 +1,95 @@
-#include <boost/smart_ptr/shared_ptr.hpp>
+//  Boost shared_ptr_example.cpp  --------------------------------------------//
 
-using boost::shared_ptr;
+//  Copyright Beman Dawes 2001.  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+
+//  See http://www.boost.org/libs/smart_ptr for documentation.
+
+//  Revision History
+//  21 May 01  Initial complete version (Beman Dawes)
+
+//  The original code for this example appeared in the shared_ptr documentation.
+//  Ray Gallimore pointed out that foo_set was missing a Compare template
+//  argument, so would not work as intended.  At that point the code was
+//  turned into an actual .cpp file so it could be compiled and tested.
+
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <boost/shared_ptr.hpp>
+
+//  The application will produce a series of
+//  objects of type Foo which later must be
+//  accessed both by occurrence (std::vector)
+//  and by ordering relationship (std::set).
+
+struct Foo
+{ 
+  Foo( int _x ) : x(_x) {}
+  ~Foo() { std::cout << "Destructing a Foo with x=" << x << "\n"; }
+  int x;
+  /* ... */
+};
+
+typedef boost::shared_ptr<Foo> FooPtr;
+
+class FooPtrOps
+{
+public:
+  const bool operator()( const FooPtr & a, const FooPtr & b )
+    { return a->x > b->x; }
+  void operator()( const FooPtr & a )
+    { std::cout << a->x << "\n"; }
+};
+
 int main()
 {
-	shared_ptr<int> p(new int(42));
+  std::vector<FooPtr>         foo_vector;
+  std::set<FooPtr,FooPtrOps>  foo_set; // NOT multiset!
 
-	//--- Example 1 ---
-	// thread A
-	shared_ptr<int> p2(p); // reads p
-	// thread B
-	shared_ptr<int> p3(p); // OK, multiple reads are safe
+  FooPtr foo_ptr( new Foo( 2 ) );
+  foo_vector.push_back( foo_ptr );
+  foo_set.insert( foo_ptr );
 
-	//--- Example 2 ---
-	// thread A
-	p.reset(new int(1912)); // writes p
-	// thread B
-	p2.reset(); // OK, writes p2
+  foo_ptr.reset( new Foo( 1 ) );
+  foo_vector.push_back( foo_ptr );
+  foo_set.insert( foo_ptr );
 
-	//--- Example 3 ---
-	// thread A
-	p = p3; // reads p3, writes p
-	// thread B
-	p3.reset(); // writes p3; undefined, simultaneous read/write
+  foo_ptr.reset( new Foo( 3 ) );
+  foo_vector.push_back( foo_ptr );
+  foo_set.insert( foo_ptr );
 
-	//--- Example 4 ---
-	// thread A
-	p3 = p2; // reads p2, writes p3
-	// thread B
-	// p2 goes out of scope: undefined, the destructor is considered a "write access"
-	
-	//--- Example 5 ---
-	// thread A
-	p3.reset(new int(1));
-	// thread B
-	p3.reset(new int(2)); // undefined, multiple writes
+  foo_ptr.reset ( new Foo( 2 ) );
+  foo_vector.push_back( foo_ptr );
+  foo_set.insert( foo_ptr );
+
+  std::cout << "foo_vector:\n";
+  std::for_each( foo_vector.begin(), foo_vector.end(), FooPtrOps() );
+  
+  std::cout << "\nfoo_set:\n"; 
+  std::for_each( foo_set.begin(), foo_set.end(), FooPtrOps() );
+  std::cout << "\n";
+
+//  Expected output:
+//
+//   foo_vector:
+//   2
+//   1
+//   3
+//   2
+//   
+//   foo_set:
+//   3
+//   2
+//   1
+//
+//   Destructing a Foo with x=2
+//   Destructing a Foo with x=1
+//   Destructing a Foo with x=3
+//   Destructing a Foo with x=2
+   
+  return 0;
 }
