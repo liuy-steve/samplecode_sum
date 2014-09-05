9f418a0a059a9cb7404185078578c6fe0f338dd6
liuyang <liuy_steve@hotmail.com>
2014-09-05 16:40:31 +0800

    增加python的cli程序 增加异常处理

---
+++ unp_py/intro/daytimecli.py
@@ -0,0 +1,32 @@
+
+import socket,sys
+
+def daytimecli(url):
+    if url == '':
+        url = '127.0.0.1'
+    try:
+        print("Server Ip = ", url)
+        print("Creating socket...")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("done.")
+
+        print("Conneting to remote host...")
+        s.connect(('10.17.59.147', 13))
+        print("done.")
+
+        while (1):
+            try:
+                buf = s.recv(1024)
+            except socket.error as e:
+                print("socket read error!")
+            if len(buf) == 0:
+                break
+            print("time is ...", buf)
+
+        print("close connection...")
+        s.close()
+        print("done.")
+    except socket.error as e:
+        print("socket error!")
+        sys.exit(1)
+    
