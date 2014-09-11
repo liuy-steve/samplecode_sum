c0085929861156fe88884fe89ff742a23ffae3cf
liuyang <liuy_steve@hotmail.com>
2014-09-11 18:17:37 +0800

    增加single number

---
+++ leetcode/single number/single_number/single_number.h
@@ -0,0 +1,15 @@
+
+
+
+class Solution {
+public:
+	int singleNumber(int A[], int n) {
+		int sum = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			sum ^= A[i];
+		}
+		return sum;
+	}
+};
---
+++ leetcode/single number/single_number_test/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "single_number.h"
+
+int main()
+{
+	Solution solution;
+	int aTest[] = {0};
+	std::cout << solution.singleNumber(aTest, 1);
+}
