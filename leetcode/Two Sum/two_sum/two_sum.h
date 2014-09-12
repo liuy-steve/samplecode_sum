c991ee57943b73e50843187ea8cf7c6d290ef5d3
liuyang <liuy_steve@hotmail.com>
2014-09-12 09:50:04 +0800

    修改最终版本

--- leetcode/Two Sum/two_sum/two_sum.h
+++ leetcode/Two Sum/two_sum/two_sum.h
@@ -1,22 +1,65 @@
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using std::vector;
 using std::find;
+using std::map;
+
+struct Node
+{
+	int num, pos;
+	Node(int n, int p)
+		:num(n)
+		,pos(p)
+	{
+
+	}
+};
+
+struct Compare_Opt
+{
+	bool operator() (const Node& i, const Node& j)
+	{
+		return i.num < j.num;
+	}
+};
+
 class Solution {
 public:
 	vector<int> twoSum(vector<int> &numbers, int target) {
-		vector<int> vRet;
-		for (vector<int>::iterator it = numbers.begin(); it != numbers.end() - 1; it++)
+		vector<Node> numbers_map;
+		vector<int> ret;
+
+		for (vector<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
 		{
-			vector<int>::iterator dest = find(it + 1, numbers.end(), target - *it);
-			if (dest != numbers.end())
+			numbers_map.push_back(Node(*it, distance(numbers.begin(), it) + 1));
+		}
+
+		sort(numbers_map.begin(), numbers_map.end(), Compare_Opt());
+
+		vector<Node>::iterator head, tail;
+		head = numbers_map.begin();
+		tail = numbers_map.end() - 1;
+
+		for (; tail != head; )
+		{
+			int sum = tail->num + head->num;
+			if (sum < target) 
 			{
-				vRet.push_back(it - numbers.begin() + 1);
-				vRet.push_back(dest - numbers.begin() + 1);
-				return vRet;
+				head ++;
+			}
+			else if (sum > target)
+			{
+				tail --;
+			}
+			else
+			{
+				ret.push_back(std::min<int>(tail->pos, head->pos));
+				ret.push_back(std::max<int>(tail->pos, head->pos));
+				return ret;
 			}
 		}
-		return vRet;
+		return ret;
 	}
 };
